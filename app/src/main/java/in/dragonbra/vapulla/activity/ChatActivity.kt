c1dd2a07e9f8f033466e54e71077be1b91e0cb06
Long Tran <lngtrn94@gmail.com>
2018-03-30 14:56:18 +0200

    hide image button while typing

--- app/src/main/java/in/dragonbra/vapulla/activity/ChatActivity.kt
+++ app/src/main/java/in/dragonbra/vapulla/activity/ChatActivity.kt
@@ -181,7 +181,12 @@ class ChatActivity : VapullaBaseActivity<ChatView, ChatPresenter>(), ChatView, T
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        if (s.isEmpty()) {
+            imageButton.show()
+        } else {
+            imageButton.hide()
+        }
     }
 
     override fun onMenuItemClick(item: MenuItem) = when (item.itemId) {
