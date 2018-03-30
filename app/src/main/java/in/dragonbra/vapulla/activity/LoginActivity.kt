a372b1300664196ef1c0866f88ec9a200631b0ac
Long Tran <lngtrn94@gmail.com>
2018-03-31 01:16:25 +0200

    logo animation

--- app/src/main/java/in/dragonbra/vapulla/activity/LoginActivity.kt
+++ app/src/main/java/in/dragonbra/vapulla/activity/LoginActivity.kt
@@ -7,8 +7,16 @@ import `in`.dragonbra.vapulla.extension.show
 import `in`.dragonbra.vapulla.presenter.LoginPresenter
 import `in`.dragonbra.vapulla.util.Utils
 import `in`.dragonbra.vapulla.view.LoginView
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.Snackbar
+import android.support.graphics.drawable.Animatable2Compat
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import kotlinx.android.synthetic.main.login_form.*
 import kotlinx.android.synthetic.main.login_loading.*
 import kotlinx.android.synthetic.main.login_retry.*
@@ -17,6 +25,7 @@ import org.jetbrains.anko.clearTask
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.newTask
 import javax.inject.Inject
+
 
 class LoginActivity : VapullaBaseActivity<LoginView, LoginPresenter>(), LoginView {
 
@@ -91,5 +100,46 @@ class LoginActivity : VapullaBaseActivity<LoginView, LoginPresenter>(), LoginVie
             steamGuardLayout.hide()
             failedLayout.show()
         }
+    }
+
+    fun startLoadingAnimation() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            val d = vapullaLogoMiddle.drawable as AnimatedVectorDrawableCompat
+            val d2 = vapullaLogoBottom.drawable as AnimatedVectorDrawableCompat
+            vapullaLogoMiddle.setImageDrawable(d)
+            d.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    d.start()
+                    Handler().postDelayed({
+                        d2.stop()
+                        d2.start()
+                    }, 300)
+                }
+            })
+            d.start()
+            Handler().postDelayed({ d2.start() }, 400)
+        } else {
+            val d = vapullaLogoMiddle.drawable as AnimatedVectorDrawable
+            val d2 = vapullaLogoBottom.drawable as AnimatedVectorDrawable
+            d.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    d.start()
+                    Handler().postDelayed({
+                        d2.stop()
+                        d2.start()
+                    }, 300)
+                }
+            })
+            d.start()
+            Handler().postDelayed({ d2.start() }, 400)
+        }
+    }
+
+    fun stopLoadingAnimation() {
+        val d = vapullaLogoMiddle.drawable as Animatable
+        val d2 = vapullaLogoBottom.drawable as Animatable
+
+        d.stop()
+        d2.stop()
     }
 }
